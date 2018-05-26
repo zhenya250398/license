756a156e2c82f9ad112858cb11bc771813fe0790
zhenya <zhenya250398@gmail.com>
2018-05-27 01:23:27 +0600

    с лицензией

---
+++ DataCollection.Web.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DataCollection.Web.UI.Startup))]
+namespace DataCollection.Web.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
